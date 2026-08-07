5d437db6f43cf68d55b438173498d86d5f914d65
David Gonzalez-Velez <davidgonzalez1019@gmail.com>
2026-08-07 20:13:30 +0000

    Preparing for blog

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const page = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const { slug } = await params;
+  return <div>page</div>;
+};
+
+export default page;
