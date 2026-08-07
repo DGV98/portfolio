99edf5d944331dbca731187c79b2b550862c9994
David Gonzalez-Velez <davidgonzalez1019@gmail.com>
2026-08-07 20:34:13 +0000

    Checkpoint

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,8 +1,24 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 const page = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const { slug } = await params;
-  return <div>page</div>;
+  try {
+    const post = await import(`@/posts/${slug}.mdx`);
+    const { default: Content, metadata } = post;
+    return (
+      <article className="prose dark:prose-invert w-full max-w-3xl mx-auto">
+        <div className="space-y-4">
+          <h1>{metadata.title}</h1>
+          <div>{metadata.publishDate}</div>
+        </div>
+        <Content />
+      </article>
+    );
+  } catch {
+    console.log(slug);
+    notFound();
+  }
 };
 
 export default page;
