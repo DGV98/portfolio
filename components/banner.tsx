941290569f458cbb3454f9935222338b167981e8
DGV98 <davidgonzalez1019@gmail.com>
2026-08-10 16:32:02 -0500

    Added banner

--- components/banner.tsx
+++ components/banner.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 // TODO: Banner for blogs
 const Banner = () => {
   return (
-    <nav className="mt-4 flex h-24 justify-center px-4 lg:absolute lg:z-10 lg:w-full lg:justify-end">
+    <nav className="mt-8 h-auto pl-8 lg:absolute lg:ml-[37.5%] lg:w-full">
       <Link href="/" className="font-terminal text-4xl">
         hellodavid<span className="text-accent">.dev</span>
       </Link>
