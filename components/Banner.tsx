b44a5a2b20c22848de39b9ed6e2abeb84b8accc0
DGV98 <davidgonzalez1019@gmail.com>
2026-08-14 14:36:43 -0500

    Fixed Banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
+
 // TODO: Banner for blogs
 const Banner = () => {
   return (
-    <div className="flex flex-col text-right text-3xl font-bold lg:text-4xl">
-      <p className="font-terminal text-banner">hellodavid</p>
-      <p className="font-terminal text-accent">.dev</p>
-      {/* <Link href="/" className="font-terminal text-3xl lg:text-4xl">
-        hellodavid<span className="text-accent">.dev</span>
-      </Link> */}
+    <div className="font-terminal flex flex-col text-right text-3xl font-bold xl:text-4xl 2xl:text-5xl">
+      <Link href="/" className="text-banner">
+        <p className="font-terminal text-banner">hellodavid</p>
+        <p className="font-terminal text-accent">.dev</p>
+      </Link>
     </div>
   );
 };
