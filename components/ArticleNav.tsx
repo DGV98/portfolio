c9b873987a29650304d511f585f1959f5384da01
David Gonzalez-Velez <davidgonzalez1019@gmail.com>
2026-08-19 21:59:26 +0000

    Added article nav

--- components/ArticleNav.tsx
+++ components/ArticleNav.tsx
@@ -6,12 +6,13 @@ import { profile } from "@/data/profile";
 const ArticleNav = async ({ slug }: { slug: string }) => {
   const posts = await getPosts();
   return (
-    <div className="flex flex-col gap-2">
-      <ul className="border-border border-b-2">
+    <div className="flex flex-col">
+      <h2 className="text-2xl font-semibold">Recent</h2>
+      <ul className="border-border mb-2 border-b-2">
         {posts.map(
           (post, i) =>
             post.slug !== slug && (
-              <li key={i} className="py-2">
+              <li key={i} className="py-2 hover:underline">
                 <Link href={post.slug}>{post.title}</Link>
               </li>
             ),
